ca395380ee2307d78b2c8386d11464f9fa9e565e
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-09 20:46:41 -0600

    add db controller and move db connection there as global var with init in main

---
+++ controller/db_controller.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/jhleakakos/who-wants-spells/model"
+	"gorm.io/gorm"
+	"log"
+)
+
+var DB *gorm.DB
+
+const arcanistFile = "../data/arcanist.csv"
+
+func InitializeDB() {
+
+	err := DB.Migrator().DropTable(&model.ArcanistSpell{})
+	if err != nil {
+		log.Print("check DropTable()")
+	}
+
+	err = DB.AutoMigrate(&model.ArcanistSpell{})
+	if err != nil {
+		log.Print("check AutoMigrate()")
+	}
+
+	arcanistSpellBook := model.LoadArcanistDataFromCSV(arcanistFile)
+	for key, spell := range *arcanistSpellBook {
+		fmt.Printf("loading %s\n", key)
+		DB.Create(&spell)
+	}
+}
--- main.go
+++ main.go
@@ -2,28 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/jhleakakos/who-wants-spells/model"
+	"github.com/jhleakakos/who-wants-spells/controller"
 	"github.com/jhleakakos/who-wants-spells/view"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
 )
 
-func main() {
-	fmt.Println(view.DisplayMenu())
-	fmt.Println()
-
-	db, err := gorm.Open(sqlite.Open("./data/spells.sqlite"), &gorm.Config{})
+func init() {
+	var err error
+	controller.DB, err = gorm.Open(sqlite.Open("./spells.sqlite"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("db connection no good")
 	}
+}
 
-	// comment or uncomment these as needed to rebuild db
-	//model.InitializeDB(db)
+func main() {
+	fmt.Println(view.PrintWelcomeScreen())
 
-	//var arcanistSpell model.ArcanistSpell
-	//db.First(&arcanistSpell)
-	var arcanistSpells []model.ArcanistSpell
-	db.Find(&arcanistSpells)
-	fmt.Println(arcanistSpells)
+	// comment or uncomment this as needed to rebuild db
+	//controller.InitializeDB()
+
+	controller.RunMenuLoop()
+
 }
