b7bb880b11f3009877657163c1eaa66b2f1b66a2
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-09 18:49:09 -0600

    add arcanist context menu

--- controller/menu_controller.go
+++ controller/menu_controller.go
@@ -1,12 +1,29 @@
 package controller
 
-import "fmt"
+func DisplayMainMenu() string {
+	return "Select choice by number:\n\n" +
+		"1  -- Arcanist Spell Book\n" +
+		"2  -- Diviner Spell Book\n\n" +
+		"10 -- Look up spell by name\n" +
+		"0  -- Quit\n\n" +
+		"Select choice:"
+}
 
-func DisplayMenu() string {
-	return fmt.Sprint("Select choice by number:\n\n" +
-		"1- Arcanist Spell Book\n" +
-		"2- Diviner Spell Book\n\n" +
-		"10- Look up spell by name\n" +
-		"0- Quit\n\n" +
-		"Select choice:")
+func DisplayArcanistMenu() string {
+	return "Select choice by number:\n\n" +
+		"1  -- Level\n" +
+		"2  -- Spell name\n" +
+		"3  -- Frequency\n" +
+		"4  -- Description\n" +
+		"5  -- Range\n" +
+		"6  -- Casting time\n" +
+		"7  -- Duration\n" +
+		"8  -- Area of Effect\n" +
+		"9  -- Damage\n" +
+		"10 -- Saving throw\n" +
+		"11 -- Reversible\n" +
+		"12 -- Components\n" +
+		"13 -- Special components\n" +
+		"0  -- Return to main menu\n\n" +
+		"Select choice:"
 }
--- controller/menu_controller_test.go
+++ controller/menu_controller_test.go
@@ -1,20 +1,46 @@
 package controller
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestDisplayMenu(t *testing.T) {
+func TestDisplayMainMenu(t *testing.T) {
 
-	got := DisplayMenu()
+	got := DisplayMainMenu()
 
 	expected := "Select choice by number:\n\n" +
-		"1- Arcanist Spell Book\n" +
-		"2- Diviner Spell Book\n\n" +
-		"10- Look up spell by name\n" +
-		"0- Quit\n\n" +
+		"1  -- Arcanist Spell Book\n" +
+		"2  -- Diviner Spell Book\n\n" +
+		"10 -- Look up spell by name\n" +
+		"0  -- Quit\n\n" +
 		"Select choice:"
 
 	if got != expected {
-		t.Error("Check your menu")
+		t.Error("check main menu")
 	}
+}
 
+func TestDisplayArcanistMenu(t *testing.T) {
+	got := DisplayArcanistMenu()
+
+	expected := "Select choice by number:\n\n" +
+		"1  -- Level\n" +
+		"2  -- Spell name\n" +
+		"3  -- Frequency\n" +
+		"4  -- Description\n" +
+		"5  -- Range\n" +
+		"6  -- Casting time\n" +
+		"7  -- Duration\n" +
+		"8  -- Area of Effect\n" +
+		"9  -- Damage\n" +
+		"10 -- Saving throw\n" +
+		"11 -- Reversible\n" +
+		"12 -- Components\n" +
+		"13 -- Special components\n" +
+		"0  -- Return to main menu\n\n" +
+		"Select choice:"
+
+	if got != expected {
+		t.Error("check arcanist spell menu")
+	}
 }
