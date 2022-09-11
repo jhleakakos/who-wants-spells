4d504d0dbb1c1ab33835323d707939b4c39d9c9e
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-10 22:43:22 -0600

    add diviner spell struct and basic print method

---
+++ model/diviner_spell.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type DivinerSpell struct {
+	gorm.Model
+
+	DivinationSchool string
+	Tier             string
+	Name             string
+	Description      string
+	Range            string
+	CastingTime      string
+	Duration         string
+	AreaOfEffect     string
+	Damage           string
+	SavingThrow      string
+	Reversible       string
+}
+
+func (s *DivinerSpell) PrintSpell() string {
+
+	sb := fmt.Sprintf(
+		"\n\n\n\n\n"+
+			"Divination school: %s\n"+
+			"Tier: %s\n"+
+			"Name: %s\n"+
+			"Range: %s\n"+
+			"CastingTime: %s\n"+
+			"Duration: %s\n"+
+			"AreaOfEffect: %s\n"+
+			"Damage: %s\n"+
+			"SavingThrow: %s\n"+
+			"Reversible: %s\n"+
+			"Description:\n\n%s\n",
+		s.DivinationSchool,
+		s.Tier,
+		s.Name,
+		s.Range,
+		s.CastingTime,
+		s.Duration,
+		s.AreaOfEffect,
+		s.Damage,
+		s.SavingThrow,
+		s.Reversible,
+		s.Description,
+	)
+
+	return sb
+}
