4add5cb7ecb0ce77a7e28d872e1fe70bd5f0ed5f
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-09-10 22:10:18 -0600

    add formatting for text wrapping at given line length for arcanist spell description

--- model/arcanist_spell.go
+++ model/arcanist_spell.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type ArcanistSpell struct {
@@ -22,6 +23,8 @@ type ArcanistSpell struct {
 	SpecialComponents string
 	Class             string
 }
+
+const maxLineLength = 85
 
 func (s *ArcanistSpell) PrintSpell() string {
 
@@ -54,12 +57,53 @@ func (s *ArcanistSpell) PrintSpell() string {
 		s.Components,
 		s.SpecialComponents,
 		s.Class,
-		s.Description,
+		s.formatDescription(),
 	)
 
 	return sb
 }
 
-func formatDescription(s string) string {
-	return s
+func (s *ArcanistSpell) formatDescription() string {
+	currentLineLength := 0
+
+	var result string
+	splitString := strings.Split(s.Description, " ")
+
+	for _, word := range splitString {
+
+		newLineSplit := []string{"", ""}
+
+		if strings.Contains(word, "\n\n") {
+			newLineIndex := strings.Index(word, "\n")
+			newLineSplit[0] = word[:newLineIndex]
+			newLineSplit[1] = word[newLineIndex+1:]
+		} else if strings.Contains(word, "\n") {
+			newLineSplit = strings.Split(word, "\n")
+		} else {
+			newLineSplit[0] = word
+		}
+
+		lenFirstWord := len(newLineSplit[0])
+		lenSecondWord := len(newLineSplit[1])
+
+		if currentLineLength+lenFirstWord <= maxLineLength {
+			result += newLineSplit[0]
+			result += " "
+			currentLineLength += lenFirstWord + 1
+		} else {
+			result += "\n"
+			result += newLineSplit[0]
+			result += " "
+			currentLineLength = lenFirstWord + 1
+		}
+
+		if newLineSplit[1] != "" {
+			result += "\n"
+			result += newLineSplit[1]
+			result += " "
+			currentLineLength = lenSecondWord + 1
+		}
+	}
+
+	return result
 }
